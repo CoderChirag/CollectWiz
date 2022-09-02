809ab763336607cc448820ebc1e3accbf19f7899
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-09-02 10:06:24 +0530

    Add UserContext

---
+++ src/contexts/user/user.context.jsx
@@ -0,0 +1,62 @@
+import { createContext, useEffect, useMemo, useReducer } from 'react';
+
+import { USER_ACTION_TYPES } from './user.types';
+import {
+	onAuthStateChangedListener,
+	createUserProfileDocument,
+} from '../../utils/firebase/firebase.util';
+
+export const UserContext = createContext({
+	currentUser: null,
+	setCurrentUser: () => {},
+});
+
+const userReducer = (state, action) => {
+	const { type, payload } = action;
+	switch (type) {
+		case USER_ACTION_TYPES.SET_CURRENT_USER:
+			return {
+				...state,
+				currentUser: payload,
+			};
+		default:
+			throw new Error(`Unhandled action type: ${type} in userReducer`);
+	}
+};
+
+const INITIAL_STATE = {
+	currentUser: null,
+};
+
+export const UserProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+	const setCurrentUser = useMemo(
+		() => user => {
+			dispatch({
+				type: USER_ACTION_TYPES.SET_CURRENT_USER,
+				payload: user,
+			});
+		},
+		[dispatch]
+	);
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChangedListener(user => {
+			if (user) {
+				createUserProfileDocument(user);
+			}
+			setCurrentUser(user);
+		});
+
+		return unsubscribe;
+	}, [setCurrentUser]);
+
+	return (
+		<UserContext.Provider
+			value={{ currentUser: state.currentUser, setCurrentUser }}
+		>
+			{children}
+		</UserContext.Provider>
+	);
+};
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import { UserProvider } from './contexts/user/user.context';
 import App from './App';
 
 import './index.css';
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<UserProvider>
+				<App />
+			</UserProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
