d88569e3c281e1cc0d649b40dd781f67b0a6d0d9
Aradhya Garg <78140726+Aradhyagarg@users.noreply.github.com>
2022-09-03 01:47:33 +0530

    Update appTitle.component.jsx

--- src/components/atoms/app-title/appTitle.component.jsx
+++ src/components/atoms/app-title/appTitle.component.jsx
@@ -2,10 +2,19 @@ import styled from '@emotion/styled';
 
 const AppHeading = styled.img`
 	width: ${props => props.width};
+	@media (max-width: 768px) {
+		width: ${props => props.widthMd};
+	}
 `;
 
-const AppTitle = ({ width }) => {
-	return <AppHeading src='./assets/images/logo.svg' width={width} />;
+const AppTitle = ({ width, widthMd }) => {
+	return (
+		<AppHeading
+			src='./assets/images/logo.svg'
+			width={width}
+			widthMd={widthMd}
+		/>
+	);
 };
 
 export default AppTitle;
