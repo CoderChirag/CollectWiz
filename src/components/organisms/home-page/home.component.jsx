1ff5b7514816d0ba3a64c6219efa5fc815c00fae
Anushka Singh <78140910+Nushcode@users.noreply.github.com>
2022-09-04 03:22:48 +0530

    Update home.component.jsx

--- src/components/organisms/home-page/home.component.jsx
+++ src/components/organisms/home-page/home.component.jsx
@@ -1,12 +1,12 @@
-import { Box } from '@mui/material';
-
 import Navbar from '../../molecules/navbar/navbar.component';
+import FilesystemArea from '../../molecules/filesystem-area/filesystemArea.component';
 import BottomSearchbar from '../../molecules/bottom-searchbar/bottomSearchbar.component';
 
 const HomePage = () => {
 	return (
 		<>
 			<Navbar />
+			<FilesystemArea />
 			<BottomSearchbar />
 		</>
 	);
