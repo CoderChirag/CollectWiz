74a9b057afe9ee196b333bd2696c7720b602476a
Anushka Singh <78140910+Nushcode@users.noreply.github.com>
2022-09-04 03:23:49 +0530

    Create filesystemArea.component.jsx

---
+++ src/components/molecules/filesystem-area/filesystemArea.component.jsx
@@ -0,0 +1,223 @@
+import { Box, Grid, Typography } from '@mui/material';
+import FolderIcon from '@mui/icons-material/Folder';
+
+const FilesystemArea = () => {
+	return (
+		<Box sx={{ padding: 8 }}>
+			<Grid container spacing={2}>
+				<Grid
+					item
+					sx={{
+						padding: '0 2rem !important',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						marginBottom: '30px',
+					}}
+				>
+					<FolderIcon
+						sx={{
+							fontSize: '5rem',
+							color: '#22a6ff',
+						}}
+					/>
+					<Typography variant='p' sx={{ fontSize: '1.2rem' }}>
+						Apps
+					</Typography>
+				</Grid>
+				<Grid
+					item
+					sx={{
+						padding: '0 2rem !important',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<FolderIcon
+						sx={{
+							fontSize: '5rem',
+							color: '#22a6ff',
+						}}
+					/>
+					<Typography variant='p' sx={{ fontSize: '1.2rem' }}>
+						Docs
+					</Typography>
+				</Grid>
+				<Grid
+					item
+					sx={{
+						padding: '0 2rem !important',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<FolderIcon
+						sx={{
+							fontSize: '5rem',
+							color: '#22a6ff',
+						}}
+					/>
+					<Typography variant='p' sx={{ fontSize: '1.2rem' }}>
+						My Test Folder
+					</Typography>
+				</Grid>
+				<Grid
+					item
+					sx={{
+						padding: '0 2rem !important',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<FolderIcon
+						sx={{
+							fontSize: '5rem',
+							color: '#22a6ff',
+						}}
+					/>
+					<Typography variant='p' sx={{ fontSize: '1.2rem' }}>
+						New Folder
+					</Typography>
+				</Grid>
+				<Grid
+					item
+					sx={{
+						padding: '0 2rem !important',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<FolderIcon
+						sx={{
+							fontSize: '5rem',
+							color: '#22a6ff',
+						}}
+					/>
+					<Typography variant='p' sx={{ fontSize: '1.2rem' }}>
+						New Folder
+					</Typography>
+				</Grid>
+				<Grid
+					item
+					sx={{
+						padding: '0 2rem !important',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<FolderIcon
+						sx={{
+							fontSize: '5rem',
+							color: '#22a6ff',
+						}}
+					/>
+					<Typography variant='p' sx={{ fontSize: '1.2rem' }}>
+						New Folder
+					</Typography>
+				</Grid>
+				<Grid
+					item
+					sx={{
+						padding: '0 2rem !important',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<FolderIcon
+						sx={{
+							fontSize: '5rem',
+							color: '#22a6ff',
+						}}
+					/>
+					<Typography variant='p' sx={{ fontSize: '1.2rem' }}>
+						New Folder
+					</Typography>
+				</Grid>
+				<Grid
+					item
+					sx={{
+						padding: '0 2rem !important',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<FolderIcon
+						sx={{
+							fontSize: '5rem',
+							color: '#22a6ff',
+						}}
+					/>
+					<Typography variant='p' sx={{ fontSize: '1.2rem' }}>
+						New Folder
+					</Typography>
+				</Grid>
+				<Grid
+					item
+					sx={{
+						padding: '0 2rem !important',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<FolderIcon
+						sx={{
+							fontSize: '5rem',
+							color: '#22a6ff',
+						}}
+					/>
+					<Typography variant='p' sx={{ fontSize: '1.2rem' }}>
+						New Folder
+					</Typography>
+				</Grid>
+				<Grid
+					item
+					sx={{
+						padding: '0 2rem !important',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<FolderIcon
+						sx={{
+							fontSize: '5rem',
+							color: '#22a6ff',
+						}}
+					/>
+					<Typography variant='p' sx={{ fontSize: '1.2rem' }}>
+						New Folder
+					</Typography>
+				</Grid>
+				<Grid
+					item
+					sx={{
+						padding: '0 2rem !important',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<FolderIcon
+						sx={{
+							fontSize: '5rem',
+							color: '#22a6ff',
+						}}
+					/>
+					<Typography variant='p' sx={{ fontSize: '1.2rem' }}>
+						New Folder
+					</Typography>
+				</Grid>
+			</Grid>
+		</Box>
+	);
+};
+
+export default FilesystemArea;
