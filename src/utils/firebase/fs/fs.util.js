52d264b852dd4a5876b2a13354373f7ae240ba32
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-09-04 12:29:54 +0530

    Add: fetchDataFromFs Utility

--- src/utils/firebase/fs/fs.util.js
+++ src/utils/firebase/fs/fs.util.js
@@ -1,6 +1,6 @@
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 
-import { db } from '../firebase.util';
+import { db, auth } from '../firebase.util';
 
 export const createNewUserFs = async uid => {
 	const fsDocRef = doc(db, 'fs', uid);
@@ -21,4 +21,26 @@ export const createNewUserFs = async uid => {
 		console.log('Error fetching user fs', e.message);
 	}
 	return fsDocRef;
+};
+
+export const fetchDataFromFs = async (uid, path) => {
+	if (!uid) return;
+	const fsDocRef = doc(db, 'fs', uid, path, 'data');
+	try {
+		const fsDoc = await getDoc(fsDocRef);
+		if (fsDoc.exists()) {
+			return fsDoc.data();
+		} else {
+			return {
+				subfolders: [],
+				files: [],
+			};
+		}
+	} catch (e) {
+		console.log('Error fetching data from fs', e.message);
+		return {
+			subfolders: [],
+			files: [],
+		};
+	}
 };
