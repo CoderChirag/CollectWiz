515a92c7d5159555d8f62e7ab142b0f9126a0963
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-09-04 11:54:48 +0530

    Fix: AppTitle Component

--- src/components/atoms/app-title/appTitle.component.jsx
+++ src/components/atoms/app-title/appTitle.component.jsx
@@ -1,19 +1,35 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 const AppHeading = styled.img`
 	width: ${props => props.width};
+	cursor: ${props => props.sx.cursor};
 	@media (max-width: 768px) {
 		width: ${props => props.widthMd};
 	}
 `;
 
-const AppTitle = ({ width, widthMd }) => {
+const AppTitle = ({ width, widthMd, sx, href }) => {
 	return (
-		<AppHeading
-			src='/assets/images/logo.svg'
-			width={width}
-			widthMd={widthMd}
-		/>
+		<>
+			{href ? (
+				<Link to={href || ''}>
+					<AppHeading
+						src='/assets/images/logo.svg'
+						width={width}
+						widthMd={widthMd}
+						sx={{ ...sx }}
+					/>
+				</Link>
+			) : (
+				<AppHeading
+					src='/assets/images/logo.svg'
+					width={width}
+					widthMd={widthMd}
+					sx={{ ...sx }}
+				/>
+			)}
+		</>
 	);
 };
 
