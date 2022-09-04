88ba26bdff6470287b3f67e589176cd11472f1b8
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-09-04 11:55:54 +0530

    Integrate MuiThemeProivder

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { UserProvider } from './contexts/user/user.context';
+import MuiThemeProvider from './theme/muiThemeProvider.theme';
+import { CssBaseline } from '@mui/material';
 import App from './App';
 
 import './index.css';
@@ -11,9 +13,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<UserProvider>
-				<App />
-			</UserProvider>
+			<MuiThemeProvider>
+				<CssBaseline />
+				<UserProvider>
+					<App />
+				</UserProvider>
+			</MuiThemeProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
